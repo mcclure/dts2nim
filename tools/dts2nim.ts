f5dd116556eff8200d8879853b1725cdd8f27a30
mcc <andi.m.mcclure@gmail.com>
2016-07-16 12:07:05 -0400

    More sensible file load

--- tools/dts2nim.ts
+++ tools/dts2nim.ts
@@ -13,21 +13,25 @@ if (commander.args.length < 1)
 if (commander.args.length > 1)
 	error("Too many files specified, at the moment the limit is 1")
 
-console.log("Processing", commander.args)
-
 let program = ts.createProgram(commander.args, {})
 
 let sourceFiles = program.getSourceFiles()
 
+if (sourceFiles.length <= 1)
+	error("File not found")
+
 let typeChecker = program.getTypeChecker()
 
 for (let sourceFile of sourceFiles) {
-	console.log(sourceFile.fileName)
-	for (let sym of typeChecker.getSymbolsInScope(sourceFile.endOfFileToken, 0xFFFFFFFF)) {
-		if (sym.name[0] != "Q")
-			continue
-		console.log("\t" + sym.name + ": " +
-    		//typeChecker.typeToString(typeChecker.getTypeOfSymbolAtLocation(sym, sourceFile.endOfFileToken) ));
-			"", sym)
-	}
+	console.log("WILL SCAN", sourceFile.fileName)
+}
+
+// Emit symbols
+let sourceFile = sourceFiles[sourceFiles.length-1]
+
+for (let sym of typeChecker.getSymbolsInScope(sourceFile.endOfFileToken, 0xFFFFFFFF)) {
+	console.log("\t" + sym.name
+		//typeChecker.typeToString(typeChecker.getTypeOfSymbolAtLocation(sym, sourceFile.endOfFileToken) ));
+		//, ":", sym
+	)
 }
